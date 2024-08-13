c4c1c9ac203801694c820e0f4ed5699520e1bc35
syedDayim <dayim1277@gmail.com>
2024-08-13 11:06:15 +0530

    Project complete

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Shuraa Workout Buddy</h1>
+          <h1>Workout Buddy</h1>
         </Link>
       </div>
     </header>
